a440e55fc36d2d900ce247d1fe5ee0cf6c21fac1
petarzarkov <pzarko1@gmail.com>
2025-05-12 10:35:31 +0300

    upload coverage artifacts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
+      thresholds: {
+        lines: 100,
+        branches: 100,
+        functions: 100,
+        statements: 100,
+      },
       provider: 'v8',
       include: ['index.ts', 'timezones.ts'],
       reporter: ['text', 'json-summary', 'json'],
